6a640ff7036aaddf9e6212851b830c22f5751b20
agustingorgni <agustingorgni25@gmail.com>
2024-03-03 18:30:26 -0300

    enhancement: rollup added

---
+++ packages/react/lib/Button-CXfAse3r.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var Button = function (_a) {
+    var label = _a.label;
+    return React.createElement("button", null, label);
+};
+
+export { Button as B };
+//# sourceMappingURL=Button-CXfAse3r.js.map
